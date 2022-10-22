59deb33aa536fe78a3829345937c267e081fa35d
gemutsruhe <gemutsruhe@cau.ac.kr>
2022-10-22 11:53:45 +0900

    first kernel compile

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Thu Oct 6 07:28:46 KST 2022"
+#define LINUX_COMPILE_BY "hyesung"
+#define LINUX_COMPILE_HOST "hyesung-VirtualBox"
+#define LINUX_COMPILER "gcc version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.1)"
---
+++ vmlinux-gdb.py
@@ -0,0 +1,1 @@
+/home/hyesung/Downloads/linux-5.4.216/scripts/gdb/vmlinux-gdb.py
